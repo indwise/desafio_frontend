8eeaaa14b5376a4678347515e6a0bf55040feaaf
juan <juanlews@hotmail.com>
2019-03-18 16:27:57 -0300

    Fixtures for shifts and production

---
+++ api/controllers/fixture/shifts.js
@@ -0,0 +1,272 @@
+
+module.exports = [{
+  'weekday' : 0,
+  'shiftName' : 'Manhã',
+  'timezone' : 'America/Sao_Paulo',
+  'productionStartTime' : '06:00',
+  'productionStartDayOffset' : 0,
+  'productionEndTime' : '14:00',
+  'productionEndDayOffset' : 0,
+  'plannedStops' : [],
+  'createdAt' : '2018-08-21T23:18:39.259Z',
+  'updatedAt' : '2018-08-21T23:18:39.259Z'
+},
+{
+  'weekday' : 1,
+  'shiftName' : 'Manhã',
+  'timezone' : 'America/Sao_Paulo',
+  'productionStartTime' : '06:00',
+  'productionStartDayOffset' : 0,
+  'productionEndTime' : '14:00',
+  'productionEndDayOffset' : 0,
+  'plannedStops' : [],
+  'createdAt' : '2018-08-21T23:18:39.259Z',
+  'updatedAt' : '2018-08-21T23:18:39.259Z'
+},
+{
+
+  'weekday' : 2,
+  'shiftName' : 'Manhã',
+  'timezone' : 'America/Sao_Paulo',
+  'productionStartTime' : '06:00',
+  'productionStartDayOffset' : 0,
+  'productionEndTime' : '14:00',
+  'productionEndDayOffset' : 0,
+  'plannedStops' : [],
+  'createdAt' : '2018-08-21T23:18:39.259Z',
+  'updatedAt' : '2018-08-21T23:18:39.259Z'
+},
+{
+
+  'weekday' : 3,
+  'shiftName' : 'Manhã',
+  'timezone' : 'America/Sao_Paulo',
+  'productionStartTime' : '06:00',
+  'productionStartDayOffset' : 0,
+  'productionEndTime' : '14:00',
+  'productionEndDayOffset' : 0,
+  'plannedStops' : [],
+  'createdAt' : '2018-08-21T23:18:39.259Z',
+  'updatedAt' : '2018-08-21T23:18:39.259Z'
+},
+{
+
+  'weekday' : 4,
+  'shiftName' : 'Manhã',
+  'timezone' : 'America/Sao_Paulo',
+  'productionStartTime' : '06:00',
+  'productionStartDayOffset' : 0,
+  'productionEndTime' : '14:00',
+  'productionEndDayOffset' : 0,
+  'plannedStops' : [],
+  'createdAt' : '2018-08-21T23:18:39.259Z',
+  'updatedAt' : '2018-08-21T23:18:39.259Z'
+},
+{
+
+  'weekday' : 5,
+  'shiftName' : 'Manhã',
+  'timezone' : 'America/Sao_Paulo',
+  'productionStartTime' : '06:00',
+  'productionStartDayOffset' : 0,
+  'productionEndTime' : '14:00',
+  'productionEndDayOffset' : 0,
+  'plannedStops' : [],
+  'createdAt' : '2018-08-21T23:18:39.259Z',
+  'updatedAt' : '2018-08-21T23:18:39.259Z'
+},
+{
+
+  'weekday' : 6,
+  'shiftName' : 'Manhã',
+  'timezone' : 'America/Sao_Paulo',
+  'productionStartTime' : '06:00',
+  'productionStartDayOffset' : 0,
+  'productionEndTime' : '14:00',
+  'productionEndDayOffset' : 0,
+  'plannedStops' : [],
+  'createdAt' : '2018-08-21T23:18:39.259Z',
+  'updatedAt' : '2018-08-21T23:18:39.259Z'
+},
+{
+
+  'weekday' : 0,
+  'shiftName' : 'Tarde',
+  'timezone' : 'America/Sao_Paulo',
+  'productionStartTime' : '14:00',
+  'productionStartDayOffset' : 0,
+  'productionEndTime' : '22:00',
+  'productionEndDayOffset' : 0,
+  'plannedStops' : [],
+  'createdAt' : '2018-08-21T23:18:39.259Z',
+  'updatedAt' : '2018-08-21T23:18:39.259Z'
+},
+{
+
+  'weekday' : 0,
+  'shiftName' : 'Noite',
+  'timezone' : 'America/Sao_Paulo',
+  'productionStartTime' : '22:00',
+  'productionStartDayOffset' : 0,
+  'productionEndTime' : '06:00',
+  'productionEndDayOffset' : 1,
+  'plannedStops' : [],
+  'createdAt' : '2018-08-21T23:18:39.259Z',
+  'updatedAt' : '2018-08-21T23:18:39.259Z'
+},
+{
+
+  'weekday' : 1,
+  'shiftName' : 'Tarde',
+  'timezone' : 'America/Sao_Paulo',
+  'productionStartTime' : '14:00',
+  'productionStartDayOffset' : 0,
+  'productionEndTime' : '22:00',
+  'productionEndDayOffset' : 0,
+  'plannedStops' : [],
+  'createdAt' : '2018-08-21T23:18:39.259Z',
+  'updatedAt' : '2018-08-21T23:18:39.259Z'
+},
+{
+
+  'weekday' : 2,
+  'shiftName' : 'Tarde',
+  'timezone' : 'America/Sao_Paulo',
+  'productionStartTime' : '14:00',
+  'productionStartDayOffset' : 0,
+  'productionEndTime' : '22:00',
+  'productionEndDayOffset' : 0,
+  'plannedStops' : [],
+  'createdAt' : '2018-08-21T23:18:39.259Z',
+  'updatedAt' : '2018-08-21T23:18:39.259Z'
+},
+{
+
+  'weekday' : 3,
+  'shiftName' : 'Tarde',
+  'timezone' : 'America/Sao_Paulo',
+  'productionStartTime' : '14:00',
+  'productionStartDayOffset' : 0,
+  'productionEndTime' : '22:00',
+  'productionEndDayOffset' : 0,
+  'plannedStops' : [],
+  'createdAt' : '2018-08-21T23:18:39.259Z',
+  'updatedAt' : '2018-08-21T23:18:39.259Z'
+},
+{
+
+  'weekday' : 4,
+  'shiftName' : 'Tarde',
+  'timezone' : 'America/Sao_Paulo',
+  'productionStartTime' : '14:00',
+  'productionStartDayOffset' : 0,
+  'productionEndTime' : '22:00',
+  'productionEndDayOffset' : 0,
+  'plannedStops' : [],
+  'createdAt' : '2018-08-21T23:18:39.259Z',
+  'updatedAt' : '2018-08-21T23:18:39.259Z'
+},
+{
+
+  'weekday' : 5,
+  'shiftName' : 'Tarde',
+  'timezone' : 'America/Sao_Paulo',
+  'productionStartTime' : '14:00',
+  'productionStartDayOffset' : 0,
+  'productionEndTime' : '22:00',
+  'productionEndDayOffset' : 0,
+  'plannedStops' : [],
+  'createdAt' : '2018-08-21T23:18:39.259Z',
+  'updatedAt' : '2018-08-21T23:18:39.259Z'
+},
+{
+
+  'weekday' : 6,
+  'shiftName' : 'Tarde',
+  'timezone' : 'America/Sao_Paulo',
+  'productionStartTime' : '14:00',
+  'productionStartDayOffset' : 0,
+  'productionEndTime' : '22:00',
+  'productionEndDayOffset' : 0,
+  'plannedStops' : [],
+  'createdAt' : '2018-08-21T23:18:39.259Z',
+  'updatedAt' : '2018-08-21T23:18:39.259Z'
+},
+{
+
+  'weekday' : 1,
+  'shiftName' : 'Noite',
+  'timezone' : 'America/Sao_Paulo',
+  'productionStartTime' : '22:00',
+  'productionStartDayOffset' : 0,
+  'productionEndTime' : '06:00',
+  'productionEndDayOffset' : 1,
+  'plannedStops' : [],
+  'createdAt' : '2018-08-21T23:18:39.259Z',
+  'updatedAt' : '2018-08-21T23:18:39.259Z'
+},
+{
+
+  'weekday' : 2,
+  'shiftName' : 'Noite',
+  'timezone' : 'America/Sao_Paulo',
+  'productionStartTime' : '22:00',
+  'productionStartDayOffset' : 0,
+  'productionEndTime' : '06:00',
+  'productionEndDayOffset' : 1,
+  'plannedStops' : [],
+  'createdAt' : '2018-08-21T23:18:39.259Z',
+  'updatedAt' : '2018-08-21T23:18:39.259Z'
+},
+{
+
+  'weekday' : 3,
+  'shiftName' : 'Noite',
+  'timezone' : 'America/Sao_Paulo',
+  'productionStartTime' : '22:00',
+  'productionStartDayOffset' : 0,
+  'productionEndTime' : '06:00',
+  'productionEndDayOffset' : 1,
+  'plannedStops' : [],
+  'createdAt' : '2018-08-21T23:18:39.259Z',
+  'updatedAt' : '2018-08-21T23:18:39.259Z'
+},
+{
+
+  'weekday' : 4,
+  'shiftName' : 'Noite',
+  'timezone' : 'America/Sao_Paulo',
+  'productionStartTime' : '22:00',
+  'productionStartDayOffset' : 0,
+  'productionEndTime' : '06:00',
+  'productionEndDayOffset' : 1,
+  'plannedStops' : [],
+  'createdAt' : '2018-08-21T23:18:39.259Z',
+  'updatedAt' : '2018-08-21T23:18:39.259Z'
+},
+{
+
+  'weekday' : 5,
+  'shiftName' : 'Noite',
+  'timezone' : 'America/Sao_Paulo',
+  'productionStartTime' : '22:00',
+  'productionStartDayOffset' : 0,
+  'productionEndTime' : '06:00',
+  'productionEndDayOffset' : 1,
+  'plannedStops' : [],
+  'createdAt' : '2018-08-21T23:18:39.259Z',
+  'updatedAt' : '2018-08-21T23:18:39.259Z'
+},
+{
+
+  'weekday' : 6,
+  'shiftName' : 'Noite',
+  'timezone' : 'America/Sao_Paulo',
+  'productionStartTime' : '22:00',
+  'productionStartDayOffset' : 0,
+  'productionEndTime' : '06:00',
+  'productionEndDayOffset' : 1,
+  'plannedStops' : [],
+  'createdAt' : '2018-08-21T23:18:39.259Z',
+  'updatedAt' : '2018-08-21T23:18:39.259Z'
+}]
