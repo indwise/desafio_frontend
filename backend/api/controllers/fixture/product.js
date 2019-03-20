ecf85bac95cb3296473257f5a97ec4c75bed46b2
Gustavo Garcia <goostavobg@gmail.com>
2019-03-20 13:52:02 -0300

    Addedd product description

---
+++ backend/api/controllers/fixture/product.js
@@ -0,0 +1,29 @@
+module.exports = [
+  {
+    "type" : "unit",
+    "productId" : "ARADGE705424",
+    "label" : "Vidro Aradge",
+    "expectedProductivity" : 1,
+    "bundleMultiplier" : 1,
+    "unitSize" : 1,
+    "unitLabel" : "peça"
+  },
+  {
+    "type" : "unit",
+    "productId" : "KERW10911611",
+    "label" : "Vidro temperado W109",
+    "expectedProductivity" : 1,
+    "bundleMultiplier" : 1,
+    "unitSize" : 1,
+    "unitLabel" : "peça"
+  },
+  {
+    "type" : "unit",
+    "productId" : "ISOR1455534",
+    "label" : "Segmento ISOR",
+    "expectedProductivity" : 1,
+    "bundleMultiplier" : 1,
+    "unitSize" : 1,
+    "unitLabel" : "peça"
+  }
+]
